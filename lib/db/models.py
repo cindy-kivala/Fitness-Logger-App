2e53bdd46552d3ce1f44e2012ecda3427bf3cee5
cindy kivala <cindykivala@gmail.com>
2025-08-25 21:28:03 +0300

    WorkoutSessions model

--- lib/db/models.py
+++ lib/db/models.py
@@ -23,3 +23,32 @@ class User:
         CURSOR.execute("INSERT INTO users (name) VALUES (?)", (name,))
         CONN.commit()
         return cls(name, CURSOR.lastrowid)
+
+class WorkoutSesssion:
+    def __init__(self, user_id, activity, duration, calories, id=None):
+        self.id = id
+        self.user_id = user_id
+        self.activity = activity
+        self.duration = duration
+        self.calories = calories
+    
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute('''
+            CREATE TABLE IF NOT EXISTS workout_sessions (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                activity TEXT,
+                duration INTEGER,
+                calories INTEGER,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            )
+        ''')
+        CONN.commit()
+    
+    @classmethod
+    def create(cls, user_id, activity, duration, calories):
+        CURSOR.execute("INSERT INTO workout_sessions(user_id, activity, duration, calories) VALUES (?, ?, ?, ?))", 
+                       (user_id, activity, duration, calories))
+        CONN.commit()
+        return cls(user_id, activity, duration,calories, CURSOR.lastrowid)
